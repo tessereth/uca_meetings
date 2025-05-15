b21caf61b42648f752521aa4be8add8efaf01505
tessereth <tessereth@yahoo.com.au>
2025-05-15 15:52:29 +1000

    Remove SQl Model and add meeting create

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,11 @@ from typing import Union
 
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from sqlmodel import Session, create_engine, select
+from pydantic import BaseModel
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import Session
 from config import settings
-from models import Meeting
+from models import Meeting, gen_short_code
 
 engine = create_engine(str(settings.POSTGRES_DSN))
 
@@ -16,12 +18,30 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 def read_root():
     return {"Hello": "World"}
 
+class CreateMeeting(BaseModel):
+    name: str
+    anonymous: bool
+
+@app.post("/api/meeting")
+def create_meeting(createMeeting: CreateMeeting):
+    # TODO: validate name
+    meeting = Meeting(
+        shortCode=gen_short_code(),
+        name=createMeeting.name,
+        anonymous=createMeeting.anonymous
+    )
+    with Session(engine) as session:
+        session.add(meeting)
+        session.commit()
+        session.refresh(meeting)
+    return meeting
+
 
 @app.post("/api/meeting/{shortCode}/join")
 def join_meeting(shortCode: str):
     with Session(engine) as session:
         meeting = select(Meeting).where(Meeting.shortCode == shortCode)
-        results = session.exec(select(Meeting).where(Meeting.shortCode == shortCode))
+        results = session.scalars(select(Meeting).where(Meeting.shortCode == shortCode))
         meeting = results.first()
         if not meeting:
             return {"error": "Unknown meeting"}, 404
--- backend/models.py
+++ backend/models.py
@@ -1,12 +1,24 @@
+import random
+import string
+from typing import Optional
 import uuid
 import datetime
-from sqlmodel import Field, SQLModel
+import sqlalchemy as sa
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+class Base(DeclarativeBase):
+     pass
 
 
-class Meeting(SQLModel, table=True):
-    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-    shortCode: str = Field(sa_column_kwargs={"name": "short_code"})
-    name: str
-    anonymous: bool
-    createdAt: datetime.datetime = Field(sa_column_kwargs={"name": "created_at"})
+class Meeting(Base):
+    __tablename__ = "meeting"
 
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, server_default=sa.text('gen_random_uuid()'))
+    shortCode: Mapped[str] = mapped_column(name="short_code")
+    name: Mapped[str]
+    anonymous: Mapped[bool]
+    createdAt: Mapped[Optional[datetime.datetime]] = mapped_column(name="created_at", server_default=sa.func.now())
+
+
+def gen_short_code():
+    return ''.join(random.choices(string.ascii_letters, k=6))
